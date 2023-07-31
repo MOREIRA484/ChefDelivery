029fff1a7651f45b33a38be8a200118f6192917f
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-07-31 09:28:32 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by leonardo Moreira on 31/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
