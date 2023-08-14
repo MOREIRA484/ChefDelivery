edad3fa837b055271f12501bc8273af842427855
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-08-14 16:22:05 -0300

    implementei uma animação no meu carrossel para ele dá um contraste de que esta sendo deslizado

--- ChefDelivery/CarouselView/CarouselTabView.swift
+++ ChefDelivery/CarouselView/CarouselTabView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct CarouselTabView: View {
     
+    @State private var currentIndex = 1
+    
+    
+    
     let ordersMock: [orderType] = [
     orderType(id: 1, name: "Banner burguer", image: "barbecue-banner"),
     orderType(id: 2, name: "Banner Prato feito", image: "brazilian-meal-banner"),
@@ -16,14 +20,29 @@ struct CarouselTabView: View {
     ]
     
     var body: some View {
-        TabView {
+        TabView(selection: $currentIndex) {
             ForEach(ordersMock) { mock in
                 CarouselView(order: mock)
+                    .tag(mock.id)
                 
             }
         }
         .frame(height: 180)
         .tabViewStyle(.page(indexDisplayMode: .always))
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { _ in
+                
+                withAnimation(.easeOut(duration: 1)) {
+                    if currentIndex > ordersMock.count {
+                        currentIndex = 1
+                    }
+                    
+                    currentIndex += 1
+                }
+                
+               
+            }
+        }
     }
 }
 
